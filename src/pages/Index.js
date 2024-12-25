1a83e0aec32c0f76af8752e9a654fb0c82e0786e
dzlotn <75255812+dzlotn@users.noreply.github.com>
2024-12-24 22:53:46 -0500

    Imported source code

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -25,8 +25,8 @@ const Index = () => (
       <p>
         {' '}
         Welcome to my website. Please feel free to read more{' '}
-        <Link to="/about">about me</Link>, or you can check out my{' '}
-        <Link to="/resume">resume</Link>, <Link to="/projects">projects</Link>,{' '}
+        <Link to="/about">about me</Link>, or you can check out my{' '},
+        <Link to="/projects">projects</Link>,{' '}
         view <Link to="/stats">site statistics</Link>, or{' '}
         <Link to="/contact">contact</Link> me.
       </p>
