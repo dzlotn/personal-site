1288a14ce4b568332c80a90ff37a314018ca3809
dzlotn <daniel@rawearthfoundation.org>
2025-06-15 00:36:34 -0400

    Revert "stuff"
    This reverts commit eddfc6a335e425436f38eb8f676cc1d9a7784a38.

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -4,23 +4,26 @@ import { Helmet } from 'react-helmet-async';
 
 import Main from '../layouts/Main';
 import EmailLink from '../components/Contact/EmailLink';
+import { index } from '../data/main';
 
 const Index = () => (
   <Main>
-    <Helmet title="About this site" />
+    <Helmet title={index.title} />
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
-          <p className="subtitle">A sleek, responsive, statically-generated react application written with modern Javascript.</p>
+          <h2 data-testid="heading"><Link to="/">{index.title}</Link></h2>
+          <p className="subtitle">{index.subtitle}</p>
         </div>
       </header>
       <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
         or you can check out my {' '}
+        <Link to="/resume">resume</Link>, {' '}
         <Link to="/projects">projects</Link>, {' '}
+        view <Link to="/stats">site statistics</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
-      <p> Source available <a href="https://github.com/dzlotn/personal-site">here</a>.</p>
+      <p> Source available <a href={index.source}> here</a>.</p>
     </article>
   </Main>
 );
