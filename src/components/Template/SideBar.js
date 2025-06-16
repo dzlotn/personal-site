82da4fcd69cc60f007d72bf439eb0048cefba0d5
dzlotn <daniel@rawearthfoundation.org>
2025-06-16 00:47:29 -0400

    2d

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -21,42 +21,46 @@ const SideBar = () => {
 
   return (
     <section id="sidebar">
-      <section id="intro">
-        <Link to="/" className="logo">
-          <img src={`${PUBLIC_URL}/images/file.jpg`} alt="" />
-        </Link>
-        <header>
-          <h2>Daniel Zlotnick</h2>
+      {(!isMobile || isHomePage) && (
+        <section id="intro">
+          <Link to="/" className="logo">
+            <img src={`${PUBLIC_URL}/images/file.jpg`} alt="" />
+          </Link>
+          <header>
+            <h2>Daniel Zlotnick</h2>
+            <p>
+              <a href="mailto:daniel.zlotnick5@gmail.com">daniel.zlotnick5@gmail.com</a>
+            </p>
+          </header>
+        </section>
+      )}
+
+      {(!isMobile || isHomePage) && (
+        <section className="blurb">
+          <h2>About</h2>
           <p>
-            <a href="mailto:daniel.zlotnick5@gmail.com">daniel.zlotnick5@gmail.com</a>
+            Hi, I&apos;m Daniel Zlotnick, a student
+            at <a href="https://www.cornell.edu/">Cornell University</a> with a passion for solving complex problems
+            and harnessing technology to create meaningful change.
+            I&apos;m currently pursuing a major in <a href="https://www.cs.cornell.edu/">Computer Science</a> and
+            a minor in <a href="https://business.cornell.edu/programs/undergraduate/minors/business-engineers/">Business</a>.
           </p>
-        </header>
-      </section>
 
-      <section className="blurb">
-        <h2>About</h2>
-        <p>
-          Hi, I&apos;m Daniel Zlotnick, a student
-          at <a href="https://www.cornell.edu/">Cornell University</a> with a passion for solving complex problems
-          and harnessing technology to create meaningful change.
-          I&apos;m currently pursuing a major in <a href="https://www.cs.cornell.edu/">Computer Science</a> and
-          a minor in <a href="https://business.cornell.edu/programs/undergraduate/minors/business-engineers/">Business</a>.
-        </p>
-
-        <ul className="actions">
-          <li>
-            {window.location.pathname.includes('/about') ? (
-              <Link to="/projects" className="button">
-                My Projects
-              </Link>
-            ) : (
-              <Link to="/about" className="button">
-                About Me
-              </Link>
-            )}
-          </li>
-        </ul>
-      </section>
+          <ul className="actions">
+            <li>
+              {window.location.pathname.includes('/about') ? (
+                <Link to="/projects" className="button">
+                  My Projects
+                </Link>
+              ) : (
+                <Link to="/about" className="button">
+                  About Me
+                </Link>
+              )}
+            </li>
+          </ul>
+        </section>
+      )}
 
       <section id="footer">
         <ContactIcons />
